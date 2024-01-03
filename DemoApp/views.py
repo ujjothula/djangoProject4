eba49d4ee0e923a46516c5ba816fa65ad860dc93
ujjothula <sureshcivil2019@gmail.com>
2024-01-03 12:39:29 +0530

    Initial commit

---
+++ DemoApp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def hello(request):
+    print("hello request url executed");
+    tt = '''
+  
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+body  {
+  background-image: url("paper.gif");
+  background-color:lightpink;
+}
+</style>
+<center>
+<h1 style="background-color:lightblue;">Aparna Construction PVT.Limited</h1><br><br>
+<form action="/action_page.php">
+  <label for="fname">Username: </label>
+  <input type="text" id="fname" name="fname"><br><br>
+  
+  <label for="lname">Password: </label>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+</center>
+</body>
+</html>
+
+
+   	''';
+    return HttpResponse(tt);
